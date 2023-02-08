7bd83b65d2605afe452861a0e6b0c9b4a946f206
Rasheed Mozaffar <rasheedkmozaffar@hotmail.com>
2023-02-08 15:57:16 +0300

    Second Nav bar design release with JavaScript

---
+++ Designs/Version2/Main.js
@@ -0,0 +1,8 @@
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.nav-list');
+
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('active');
+}
